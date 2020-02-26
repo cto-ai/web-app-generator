e3c63ef7a3669727bc3bb9ac91075e3f904fbdd4
Mario Mejia <jmariomejiap@gmail.com>
2020-02-26 11:11:54 -0800

    replace cto.ai banner

--- src/utils/insertBanner.ts
+++ src/utils/insertBanner.ts
@@ -2,7 +2,7 @@ import { sdk, ux } from '@cto.ai/sdk';
 import { track } from '../utils/tracker';
 
 const bannerLink =
-  '![](https://raw.githubusercontent.com/cto-ai/aws/master/assets/banner.png)';
+  '![official-banner-v3](https://user-images.githubusercontent.com/22829270/75378425-8053e600-5888-11ea-85c2-c4f6f90d6f72.png)';
 
 export const insertBanner = async (projectName: string) => {
   try {
